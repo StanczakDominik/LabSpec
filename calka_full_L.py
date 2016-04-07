aa4a5d5aced1340bda55821937d002f40426398c
Dominik Stańczak <stanczakdominik@gmail.com>
2016-04-07 06:13:29 +0200

    clka full

--- calka_full_L.py
+++ calka_full_L.py
@@ -17,9 +17,6 @@ x = y = np.arange(nx)
 z = np.arange(nz)
 X,Y,Z = np.meshgrid(x,y,z)
 NX, NY, NZ = nx, ny, nz
-# X -= int(NX/2)
-# Y -= int(NY/2)
-# Z -= int(NZ/2)
 
 calki_po_przekrojach = np.zeros(nz)
 with h5py.File("data.hdf5", "r+") as f:
@@ -27,6 +24,9 @@ with h5py.File("data.hdf5", "r+") as f:
     grady_pointer = f['grady']
     gradz_pointer = f['gradz']
     psi_pointer = f['psi']
+    Lx_pointer = f.create_dataset(name="Lx", shape=(nx,ny,nz), dtype=float)
+    Ly_pointer = f.create_dataset(name="Ly", shape=(nx,ny,nz), dtype=float)
+    Lz_pointer = f.create_dataset(name="Lz", shape=(nx,ny,nz), dtype=float)
     for k in range(nz):
         print(k)
         grad_x = gradx_pointer[:,:,k]
@@ -36,11 +36,14 @@ with h5py.File("data.hdf5", "r+") as f:
         psi_Lz_psi = (-1j*(X[:,:,k]*grad_y - Y[:,:,k]*grad_x)*psi.conjugate()).real
         psi_Lx_psi = (-1j*(Y[:,:,k]*grad_z - Z[:,:,k]*grad_y)*psi.conjugate()).real
         psi_Ly_psi = (-1j*(Z[:,:,k]*grad_x - X[:,:,k]*grad_z)*psi.conjugate()).real
+        Lx_pointer[:,:,k] = psi_Lx_psi
+        Ly_pointer[:,:,k] = psi_Ly_psi
+        Lz_pointer[:,:,k] = psi_Lz_psi
         L = np.hypot(psi_Lz_psi, psi_Lx_psi, psi_Ly_psi)
 
         Intgr_L_x = scint.simps(L, axis=0)
-        Intgr_L_x_y = scint.simps(L, axis=0)
-        print(Intgr_L_x_y.dtype, Intgr_L_x_y.shape)
+        Intgr_L_x_y = scint.simps(Intgr_L_x, axis=0)
+        print(L.dtype, L.shape)
 
         calki_po_przekrojach[k] = Intgr_L_x_y.real
     calki_przekrojowe = f.create_dataset('calki_full', data=calki_po_przekrojach)
